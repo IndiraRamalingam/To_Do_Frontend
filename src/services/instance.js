425c37da611a71828392d71d874138ded0e25d3f
IndiraRamalingam <indiraramalingam93@gmail.com>
2023-10-24 19:03:47 +0530

    Completed

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const baseURL='https://socket-chat-f9ii.onrender.com/api'
-const baseURL='http://localhost:3001/api'
+const baseURL='https://to-do-app-maez.onrender.com/api'
+//const baseURL='http://localhost:3001/api'
 
 const authInstance= axios.create({
     baseURL : baseURL,
